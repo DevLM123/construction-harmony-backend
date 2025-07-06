14f87f546cdb974f0722b684462bd8033b40b194
prudhvi251 <prudhvimyadam@gmail.com>
2025-07-06 00:35:09 -0400

    added console log

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,10 @@ const { saveGoogleTokens } = require("../services/supabaseService");
 
 exports.getAuthUrl = (req, res) => {
   const scopes = ["https://www.googleapis.com/auth/calendar.events"];
+  console.log("Google OAuth ENV in use:", {
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+  });
   const url = googleOAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: scopes,
